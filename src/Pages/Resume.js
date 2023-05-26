efb4ba5279f78c3ce3c18fed42811c8c4b9d5d41
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-05-26 21:58:12 +1000

    Resume page and assests added

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import pdf from '../assets/RESUME p.pdf'
+import '../Styles/Resume.css'
 function Resume() {
   return (
-    <div>Resume</div>
+    <div>
+        <iframe className= "mypdf" src={pdf}></iframe>
+    </div>
   )
 }
 
