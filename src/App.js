0a388f31723daa6a40458a309be92d9a0e1b5b34
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-05-26 21:37:41 +1000

    Pages added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
 import './App.css';
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom' 
+import Home from './Pages/Home'
+import Projects from './Pages/Projects'
+import Resume from './Pages/Resume'
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+  
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/resume" element={<Resume />}/>
+            <Route path="/projects" element={<Projects />}/>
+        </Routes>
+        
+      </Router>
     </div>
   );
 }
